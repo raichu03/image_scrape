fd6354b7e081e3f3fba5f585aacae33868bb4ac6
Ansan <anshanpool@gmail.com>
2023-04-17 21:56:45 +0545

    code for scraping images

---
+++ imgdown/imgdown/spiders/main.py
@@ -0,0 +1,24 @@
+import scrapy
+
+
+class imageSpider(scrapy.Spider):
+    name = "imgdown"
+    page = 2
+    start_urls = ['https://www.123rf.com/stock-photo/wild_elephants.html?page=1']
+
+    def parse(self, response):
+        raw_image_urls = response.css('.ImageThumbnail__link img::attr(src)').getall()
+        clean_image_urls = []
+
+        for img_url in raw_image_urls:
+            clean_image_urls.append(response.urljoin(img_url))
+
+
+        yield{
+            'image_urls' : clean_image_urls
+        }
+
+        next_page = 'https://www.123rf.com/stock-photo/wild_elephants.html?page=' + str(imageSpider.page)
+        if imageSpider.page <= 100:
+            imageSpider.page += 1
+            yield response.follow(next_page, callback=self.parse)
